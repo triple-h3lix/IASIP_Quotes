463e2a4a07567ba6f0395d9ae2394cee225a2ecc
Branden M. Ardelean <branden.m.ardelean@gmail.com>
2023-02-22 20:47:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-// TODO: create .json file that contains quotes and character name, get random quote from .json file, display inside div, and based on character name, change background of the page to an image of the source character
+// Array of quotes from Its Always Sunny in Philadelphia
 
 const quotes = [
-  {
-    quote:
-      "Roxy, God Bless you. You were a good whore, you serviced me like no other whore ever did; not only my crank, but my heart; and I`m gonna miss you. AMEN.",
-    character: "Frank Reynolds",
-  },
-
-  {
-    quote: "Block the wind while I roast this bone.",
-    character: "Frank Reynolds",
-  },
-
-  {
-    quote:
-      "I don’t know how many years I got left on this planet, I’m going to get real weird with it.",
-    character: "Frank Reynolds",
-  },
-
   {
     quote: "I eat stickers all the time, dude!",
     character: "Charlie Kelly",
   },
-
   {
-    quote:
-      "What is this word 'spa'? I feel like you're starting to say a word and you're not finishing it. Are you trying to say 'Spagetti'? Are you taking me for a spaghetti day?",
+    quote: "Why don't I strap on my job helmet and squeeze down into a job cannon and fire off into job land, where jobs grow on jobbies?",
     character: "Charlie Kelly",
   },
-
   {
-    quote:
-      "Alright well I'm gonna check it out anyway, there could be something delicious in here that wasps do make and I want that.",
+    quote: "Bro, I can handle my sedatives.",
     character: "Charlie Kelly",
   },
-
   {
-    quote:
-      "Look at the door, dude. See that door there? The one marked 'Pirate'? You think a pirate lives in there?",
-    character: "Charlie Kelly",
+    quote: "I do backflips every single day of my life.",
+    character: "Mac",
   },
-];
+  {
+    quote: "I'm not fat. I'm cultivating mass.",
+    character: "Mac",
+  },
+  {
+    quote: "Let me tell you something. I haven't even BEGUN to peak. And when I do peak... you'll know it.",
+    character: "Dennis Reynolds",
+  },
+  {
+    quote: "I am the Golden God!",
+    character: "Dennis Reynolds",
+  },
+  {
+    quote: "Groban likes his ladies to pop.",
+    character: "Dee Reynolds",
+  },
+  {
+    quote: "I'll burn you alive like the last bitch who crossed me!",
+    character: "Dee Reynolds",
+  },
+  {
+    quote: "Its like when I'm doing good in the game, I'm doing good in real life",
+    character: "Dee Reynolds"
+  }
+]
+
 
 function randInt(max) {
   let n = Math.floor(Math.random() * quotes.length);
@@ -75,26 +77,35 @@ function changeText() {
   let random = quotes[randInt()];
 
   document.getElementById("quote").innerText = random.quote;
-  document.getElementById("name").innerHTML =
+  document.getElementById("character").innerHTML =
     "<h3> - <em>" + random.character + "<em><h3>";
 }
 
-function changeBG() {
-  let random = quotes[randInt()];
+function changeBG(name) {
 
-  if (random.character == "Frank Reynolds") {
-    document.body.style.backgroundImage = "url('frank.jpg')";
-  } else if (random.character == "Charlie Kelly") {
-    document.body.style.backgroundImage = "url('charlie.jpg')";
+  if (name == "Charlie Kelly") {
+    document.body.style.backgroundImage = "url('images/charlie.jpg')";
+  } else if (name == "Mac") {
+    document.body.style.backgroundImage = "url('images/mac.jpg')";
+  }
+  else if (name == "Dennis Reynolds") {
+    document.body.style.backgroundImage = "url('images/dennis.jpg')";
+  }
+  else if (name == "Dee Reynolds") {
+    document.body.style.backgroundImage = "url('images/dee.jpg')";
+  }
+  else if (name == "Frank Reynolds") {
+    document.body.style.backgroundImage = "url('images/frank.jpg')";
   }
 }
 
 // function that fades out the current quote and fades in the new quote
 function changeQuote() {
   let quote = document.getElementById("quote");
-  let name = document.getElementById("name");
+  let name = document.getElementById("character");
 
   changeText();
 
-  changeBG();
+  changeBG(document.getElementById("character").innerText);
+
 }
